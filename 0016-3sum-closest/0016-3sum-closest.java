d3d4aba46caddad12548a364f616c56a0b4709b7
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-08-24 00:54:58 +0600

    Time: 12 ms (96.03%), Space: 43.1 MB (57.21%) - LeetHub

---
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+        Arrays.sort(nums);
+        int closest = 0, distance = Integer.MAX_VALUE;
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j=i+1, k=nums.length-1;
+            while(j<k){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(Math.abs(target - sum) < distance){
+                    // while(j<k && nums[j] == nums[j+1]) j++;
+                    // while(j<k && nums[k] == nums[k-1]) k--;
+                    distance = Math.abs(target - sum);
+                    closest = sum;
+                } else if(sum == target) {
+                    return sum;
+                } else if(sum<target) {
+                    j++;
+                } else {
+                    k--;
+                }
+            }    
+        }
+        return closest;
+    }
+}
