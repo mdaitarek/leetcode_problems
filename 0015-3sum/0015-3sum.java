6c0f0c5d915dfa4959a5e4686cdfc21f02eba7eb
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-08-23 23:44:15 +0600

    Time: 32 ms (81.11%), Space: 50.7 MB (69.93%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,26 +2,24 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
         List<List<Integer>> arr = new ArrayList<List<Integer>>();
-        HashSet<String> set = new HashSet<String>();
-        int i=0;
-        while(i<nums.length-1){
-            int j = i+1,k=nums.length-1;
+        
+        for(int i=0; i<nums.length-2; i++){
+            if(i>0 && nums[i] == nums[i-1]) continue;
+            int j=i+1, k=nums.length-1;
             while(j<k){
-                if(nums[i]+nums[j]+nums[k] < 0){
+                int sum = nums[i]+nums[j]+nums[k];
+                if(sum == 0) {
+                    arr.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    while(j<k && nums[j] == nums[j+1]) j++;
+                    while(j<k && nums[k] == nums[k-1]) k--;
                     j++;
-                } else if(nums[i]+nums[j]+nums[k] > 0){
                     k--;
+                } else if(sum<0) {
+                    j++;
                 } else {
-                    String key = String.valueOf(nums[i])+String.valueOf(nums[j])+String.valueOf(nums[k]);
-                    if(!set.contains(key)){
-                        set.add(key);
-                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
-                        arr.add(list);
-                    }
-                    j++; k--;
+                    k--;
                 }
-            }
-            i++;
+            }    
         }
         return arr;
     }
