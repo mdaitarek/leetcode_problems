d15b70513c23557fc8b3e415b54278bd130818ff
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-08-25 14:58:06 +0600

    Time: 104 ms (60.96%), Space: 46.8 MB (95.19%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.js
@@ -3,13 +3,15 @@
  * @return {number}
  */
 var findLHS = function(nums) {
-    const map = {};
-    let max=0;
-    for(const num of nums) 
-        map[num] ? map[num]++ : map[num] = 1;
-    for(const item in map) 
-        if(map[parseInt(item)+1]) 
-            max = Math.max(max, map[item]+map[parseInt(item)+1]);
-    
+    nums = nums.sort((a, b) => a - b);
+    let max=0, l=0, r=1,n=nums.length;
+    while(r<nums.length){
+        let diff = nums[r] - nums[l];
+        if(diff === 1){
+            max = Math.max(max, (r-l+1));
+        }
+        if(diff <= 1) r++;
+        else l++;
+    }
     return max;
 };
