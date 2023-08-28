39dcc3b56504839013419600bb8dafdfab6480e0
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-08-28 22:51:45 +0600

    Time: 2 ms (99.64%), Space: 41.4 MB (98.99%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,21 +1,43 @@
 class Solution {
-    public boolean isCharOrDigit(char ch) {
-        return (48 <= ch && ch <= 57) 
-        || (65 <= ch && ch <= 90) 
-        || (97 <= ch && ch <= 122);
-    }
+    /**
+        My Solution
+     */
+    // public boolean isCharOrDigit(char ch) {
+    //     return (48 <= ch && ch <= 57) 
+    //     || (65 <= ch && ch <= 90) 
+    //     || (97 <= ch && ch <= 122);
+    // }
+
+    // public boolean isPalindrome(String s) {
+    //      if (s.isEmpty()) {
+    //         return true;
+    //     }
+    //     int i=0, j=s.length()-1;
+    //     while(i<=j){
+    //         if(!isCharOrDigit(s.charAt(i))) i++;
+    //         else if(!isCharOrDigit(s.charAt(j))) j--;
+    //         else if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+    //         else {
+    //             i++; j--;
+    //         }
+    //     }
+    //     return true;
+    // }
 
     public boolean isPalindrome(String s) {
-         if (s.isEmpty()) {
-            return true;
-        }
-        int i=0, j=s.length()-1;
-        while(i<=j){
-            if(!isCharOrDigit(s.charAt(i))) i++;
-            else if(!isCharOrDigit(s.charAt(j))) j--;
-            else if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) return false;
+        if(s.isEmpty()) return true;
+        int left=0, right=s.length() -1;
+        
+        while(left<=right){
+            char leftChar = s.charAt(left);
+            char rightChar = s.charAt(right);
+
+            if(!Character.isLetterOrDigit(leftChar)) left++;
+            else if(!Character.isLetterOrDigit(rightChar)) right--;
+            else if(Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) return false;
             else {
-                i++; j--;
+                left++; 
+                right--;
             }
         }
         return true;
