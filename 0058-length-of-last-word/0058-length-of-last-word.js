76400fe1dbd19d7b8ac9c08f7da9735d8aa2edc0
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-08-29 23:43:15 +0600

    Time: 58 ms (35.60%), Space: 42.3 MB (14.10%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -3,13 +3,27 @@
  * @return {number}
  */
 var lengthOfLastWord = function(s) {
-    const words = s.split(" ");
-    let len=0;
-    for(let i=words.length-1; i>=0; i--){
-        if(words[i]!=""){
-            len = words[i].length;
-            break;
-        }
+    /**
+    * My Solution 
+    */
+    // const words = s.split(" ");
+    // let len=0;
+    // for(let i=words.length-1; i>=0; i--){
+    //     if(words[i]!=""){
+    //         len = words[i].length;
+    //         break;
+    //     }
+    // }
+    // return len;
+    
+     /**
+     * Best Solution 
+     */
+    const str = s.trim();
+    let len = 0;
+    for(let i=str.length-1; i>=0; i--){
+        if(str[i]!==' ') len++;
+        else break;
     }
     return len;
 };
