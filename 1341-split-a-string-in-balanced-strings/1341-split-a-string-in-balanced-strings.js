ece30e885df7907223a56a4804fdb65579323600
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-01 22:40:30 +0600

    Time: 57 ms (46.84%), Space: 42.5 MB (27.09%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.js
@@ -1,13 +1,21 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var balancedStringSplit = function(s) {
-    let chC = 0, maxSub = 0;
-    for(let i=0; i<s.length; i++) {
-        if(s[i] === 'R') chC++;
-        else chC--;
-        if(chC === 0) maxSub++;
+const checkBalanced = (string) => {
+    let count = 0;
+    for (let i = 0; i < string.length; i++) {
+        if (string[i] === "R") count++;
     }
-    return maxSub;
+    return count === string.length/2;
+};
+
+var balancedStringSplit = function(s) {
+    let output = 1;
+    for (let i = 2; i < s.length; i += 2) {
+        if (checkBalanced(s.slice(0, i))) {
+            const newValue = 1 + balancedStringSplit(s.slice(i, s.length));
+            if (newValue > output) {
+                output = newValue;
+                break;
+            }
+        }
+    }
+    return output;
 };
