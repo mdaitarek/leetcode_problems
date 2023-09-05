c9c5dcdabf7462c1d46097fc0992a68eb707d33e
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-05 23:50:04 +0600

    Time: 64 ms (67.69%), Space: 44.1 MB (24.91%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.js
+++ 0069-sqrtx/0069-sqrtx.js
@@ -5,10 +5,8 @@
 var mySqrt = function(x) {
     let low=1, high = Math.ceil(x/2);
     let mid = Math.floor((high+low)/2);
-    if( x === 0 ) return 0;
-    if( x === 1) return 1;
+    if( x === 0 || x === 1) return x;
     while(low<=high){
-        // console.log({low, mid, high});
         if((low*low) === x) return low;
         else if((mid*mid) === x) return mid;
         else if((high*high) === x) return high;
