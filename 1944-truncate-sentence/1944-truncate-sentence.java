e23c65231de47c36bc92b5497f28f7857c3efc76
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-12 00:07:08 +0600

    Time: 1 ms (82.43%), Space: 41 MB (49.88%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -4,7 +4,7 @@ class Solution {
         StringBuilder str = new StringBuilder();
         for(int i=0; i<k; i++){
             if(i<arr.length)
-                str.append(arr[i] + " ");
+                str.append(arr[i]).append(" ");
         }
         return str.toString().trim();
     }
