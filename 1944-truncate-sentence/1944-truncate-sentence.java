608d2c0e88cb0d43328ad31f2a9452cdd7bb7ab4
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-12 00:06:29 +0600

    Time: 1 ms (82.43%), Space: 40.9 MB (57.2%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.java
+++ 1944-truncate-sentence/1944-truncate-sentence.java
@@ -2,9 +2,9 @@ class Solution {
     public String truncateSentence(String s, int k) {
         String[] arr = s.split(" ");
         StringBuilder str = new StringBuilder();
-        int n = Math.min(k, arr.length);
-        for(int i=0; i<n; i++){
-            str.append(arr[i] + " ");
+        for(int i=0; i<k; i++){
+            if(i<arr.length)
+                str.append(arr[i] + " ");
         }
         return str.toString().trim();
     }
