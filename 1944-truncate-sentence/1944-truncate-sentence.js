f0215b4c68d274ceceab0c4c7a8861db2b33ac4b
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-11 23:30:35 +0600

    Time: 57 ms (31.82%), Space: 42.3 MB (17.04%) - LeetHub

--- 1944-truncate-sentence/1944-truncate-sentence.js
+++ 1944-truncate-sentence/1944-truncate-sentence.js
@@ -4,11 +4,14 @@
  * @return {string}
  */
 var truncateSentence = function(s, k) {
-    let str = "";
-    for(const char of s){
-        if(char === " ") k--;
-        if(k === 0) break;
-        str+=char;
-    }
-    return str;
+    // let str = "";
+    // for(const char of s){
+    //     if(char === " ") k--;
+    //     if(k === 0) break;
+    //     str+=char;
+    // }
+    // return str;
+    const str = s.split(" ");
+    if(k>=str) return s;
+    return str.filter((element, index) => index < k ).join(" ");
 };
