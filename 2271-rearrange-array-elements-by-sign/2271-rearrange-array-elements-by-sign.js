acc14be759e6b853f6a94122bd09ee024d028fa0
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-22 15:17:36 +0600

    Time: 301 ms (36.05%), Space: 95.5 MB (68.64%) - LeetHub

---
+++ 2271-rearrange-array-elements-by-sign/2271-rearrange-array-elements-by-sign.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    const result = [];
+        let positive = 0, negative = 1;
+        for(const num of nums){
+            if(num > 0){
+                result[positive] = num;
+                positive+=2;
+            } else {
+                result[negative] = num;
+                negative+=2;
+            }
+        }
+        return result;
+};
