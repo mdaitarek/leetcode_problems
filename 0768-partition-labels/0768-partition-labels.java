592529a206e9926e3f2898f95be219354f7205a8
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-25 18:01:14 +0600

    Time: 9 ms (39.34%), Space: 41 MB (80.06%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,20 +1,25 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        List<Integer> labels = new ArrayList<>();
-        int start = 0, end = 0, endIndex = 0;
-
-        for(int i=0; i<s.length(); i++){
-           map.put(s.charAt(i), i);
+        Map<Character, Integer> map = new HashMap<>();
+        // filling impact of character's
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            map.put(ch, i);
         }
-        while(end < s.length()){
-            endIndex = Math.max( map.get(s.charAt(end)), endIndex);
-            if(endIndex == end){
-                labels.add((end - start + 1));
-                start = end + 1;
+        // making of result
+        List<Integer> res = new ArrayList<>();
+        int prev = -1;
+        int max = 0;
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            max = Math.max(max, map.get(ch));
+            if(max == i){
+                // partition time
+                res.add(max - prev);
+                prev = max;
             }
-            end++;              
         }
-        return labels;                
+        return res;
     }
 }
