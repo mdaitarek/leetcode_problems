635878756f1476090310035a7183b2bffbbc493f
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-25 18:03:53 +0600

    Time: 5 ms (77.2%), Space: 40.6 MB (98.32%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.java
+++ 0768-partition-labels/0768-partition-labels.java
@@ -1,22 +1,20 @@
 class Solution {
-      public static List<Integer> partitionLabels(String s) {
+    public List<Integer> partitionLabels(String s) {
+        int[] map = new int[26];
+        List<Integer> labels = new ArrayList<>();
+        int start = 0, end = 0, endIndex = 0;
 
-        int lastIndex[] = new int[26];
-        List<Integer> result = new ArrayList<>();
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            lastIndex[ch - 'a'] = i;
+        for(int i=0; i<s.length(); i++){
+           map[s.charAt(i) - 'a'] = i;
         }
-        int previousPartitionEnd = -1;
-        int maxPartitionLen = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            maxPartitionLen = Math.max(maxPartitionLen, lastIndex[ch - 'a']);
-            if (maxPartitionLen == i) {
-                result.add(maxPartitionLen - previousPartitionEnd);
-                previousPartitionEnd = maxPartitionLen;
+        while(end < s.length()){
+            endIndex = Math.max(map[s.charAt(end) - 'a'], endIndex);
+            if(endIndex == end){
+                labels.add((end - start + 1));
+                start = end + 1;
             }
+            end++;              
         }
-        return result;
+        return labels;                
     }
 }
