040eb2f74355369f858ec61cef046079c908c5e1
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-29 11:23:47 +0600

    Time: 56 ms (32.66%), Space: 43 MB (13.97%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.js
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.js
@@ -6,15 +6,11 @@
 var mergeAlternately = function(word1, word2) {
     let mergedWord = "";
     let len1 = word1.length, len2 = word2.length, i = 0;
-    let n = Math.max(len1, len2);
-    while(i<n){
-        if(i<len1 && i<len2)
-            mergedWord+=(word1[i] + word2[i]);
-        else if(i>=len1 && i<len2)
-            mergedWord+=word2[i];
-        else if(i<len1 && i>=len2)
+    while(i<len1 || i<len2){
+        if(i<len1)
             mergedWord+=word1[i];
-
+        if(i<len2)
+            mergedWord+=word2[i];
         i++;
     }
 
