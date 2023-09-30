7aba798d194f004e6d05e2faa5065234c6d89535
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-09-30 11:06:21 +0600

    Time: 1 ms (93.1%), Space: 40.4 MB (81.81%) - LeetHub

--- 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
+++ 1987-substrings-of-size-three-with-distinct-characters/1987-substrings-of-size-three-with-distinct-characters.java
@@ -1,28 +1,13 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-        int n = s.length();
-        if(n<3) return 0;
-        int[] map = new int[26];
-        int total = 0, count = 0;
-        for(int i=0; i<3; i++){
-            if(++map[s.charAt(i) - 'a']> 1)
-                ++count;
+        int count=0;
+
+        for(int i=0;i<s.length()-2;i++){
+            if((s.charAt(i)!=s.charAt(i+1))&&(s.charAt(i)!=s.charAt(i+2))&&(s.charAt(i+2)!=s.charAt(i+1))){
+                count++;
+            }
         }
+        return count;
 
-        if(count==0) total++;
-
-        for(int i=3; i<n; i++){
-            --map[s.charAt(i-3) - 'a'];
-            ++map[s.charAt(i) - 'a'];
-            if(
-                map[s.charAt(i-2) - 'a'] > 1 || 
-                map[s.charAt(i-1) - 'a'] > 1 || 
-                map[s.charAt(i) - 'a'] > 1
-            ) continue;
-
-            total++; 
-        }
-
-        return total;
     }
 }
