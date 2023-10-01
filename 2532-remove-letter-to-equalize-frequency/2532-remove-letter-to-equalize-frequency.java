f7746cf84305efbce54fe0c1b003d4b76d3ed6a8
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-10-02 00:41:33 +0600

    Time: 0 ms (100%), Space: 40 MB (95.34%) - LeetHub

--- 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.java
+++ 2532-remove-letter-to-equalize-frequency/2532-remove-letter-to-equalize-frequency.java
@@ -1,32 +1,31 @@
 class Solution {
-    public boolean equalFrequency(String word) {
-       int[] freq= new int[26];
-        for(char c : word.toCharArray()) freq[c - 'a']++;
-        
-        for(char c : word.toCharArray()){
-            freq[c - 'a']--; //as we have asked to delete exactly one char, so we will decrease freq by one for all
-                             //chars and will check if after decreasing, the freqs of all chars are same or not, If
-                             //not return false otherwise true;
-            
-            if(check(freq)){ // freqs are same no need to check further return true;
-                return true;
+    public boolean validCheck(int[] map){
+        int lastVal = 0;
+        for(int count : map){
+            if(count > 0){
+                if(lastVal == 0) lastVal = count;
+                if(count != lastVal) return false;
             }
-            freq[c - 'a']++; // we are here cause the earlier char freq is not same to other freqs. restore that char freq and check for next char.
         }
-        return false;
-    }
-    
-    public boolean check(int[] freq){
-        int count = 0;
-        
-        for(int f : freq){
-            if(f == 0) continue;
-            else if(count == 0){
-                count = f;
-            }else if(count == f) continue;
-            else return false;
-        }
-        
+
         return true;
+    }
+
+    public boolean equalFrequency(String word) {
+        int[] map = new int[26];
+
+        for(int i=0; i<word.length(); i++){
+            map[word.charAt(i) - 'a']++;
+        }
+
+        for(int i=0; i<word.length(); i++){
+            map[word.charAt(i) - 'a']--;
+            if(validCheck(map)) 
+                return true;
+
+            map[word.charAt(i) - 'a']++;
+        }
+
+        return false;
     }
 }
