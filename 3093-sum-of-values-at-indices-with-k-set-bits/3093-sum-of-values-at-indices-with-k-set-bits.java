e7ecae68f14b0c20da1842755263e2ae80af586c
Md. Ariful Islam Tarek <55447208+mdaitarek@users.noreply.github.com>
2023-10-07 23:24:59 +0600

    Time: 1 ms (100%), Space: 43.2 MB (64.6%) - LeetHub

--- 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
+++ 3093-sum-of-values-at-indices-with-k-set-bits/3093-sum-of-values-at-indices-with-k-set-bits.java
@@ -1,20 +1,11 @@
 class Solution {
-    public boolean vaildIndex(int index, int k){
-        int count = 0;
-        while(index >0){
-            if(index%2 == 1) count++;
-            if(count > k) return false;
-            index/=2;
-        }
-
-        return count == k;
-    }
     public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
-        int sum = 0;
-        for(int i=0; i<nums.size(); i++){
-            if(vaildIndex(i,k)) sum+=nums.get(i);
+        int sum=0;
+        for(int i=0; i<nums.size();i++){
+            if(Integer.bitCount(i)==k){
+                sum+=nums.get(i);
+            }
         }
-
         return sum;
     }
 }
